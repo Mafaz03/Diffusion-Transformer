9db8b88dda2285d651d2aab9300afe1b41af53ea
Mafaz03 <mohdmafaz200303@gmail.com>
2026-06-11 12:54:48 +0530

    bug fixed

--- Scheduler.py
+++ Scheduler.py
@@ -10,16 +10,16 @@ class DDPM:
 
     """
 
-    def __init__(self, betas_start, betas_end, max_timesteps: int = 1000):
+    def __init__(self, betas_start, betas_end, max_timesteps: int = 1000, device:str = "cuda"):
         self.max_timesteps = max_timesteps
-        self.betas = torch.linspace(betas_start, betas_end, max_timesteps) 
+        self.betas = torch.linspace(betas_start, betas_end, max_timesteps).to(device) 
 
         self.alphas = 1. - self.betas
 
-        self.alpha_bars_cumprod = torch.cumprod(self.alphas, dim=0)
+        self.alpha_bars_cumprod = torch.cumprod(self.alphas, dim=0).to(device)
 
-        self.alpha_bars_sqrt          = torch.sqrt(self.alpha_bars_cumprod)
-        self._1_minus_alpha_bars_sqrt = torch.sqrt(1 - self.alpha_bars_cumprod)
+        self.alpha_bars_sqrt          = torch.sqrt(self.alpha_bars_cumprod).to(device)
+        self._1_minus_alpha_bars_sqrt = torch.sqrt(1 - self.alpha_bars_cumprod).to(device)
 
     def add_noise(self, x0: torch.Tensor, t: torch.Tensor):
         # x0: [B, C, H, W]
