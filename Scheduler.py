141579811d169b7d629260e7266478f980009e24
Mafaz03 <mohdmafaz200303@gmail.com>
2026-06-12 10:11:37 +0530

    remove noise from scheduler

--- Scheduler.py
+++ Scheduler.py
@@ -29,7 +29,28 @@ class DDPM:
         x_t = (self.alpha_bars_sqrt[t].view(-1, 1, 1, 1).to(x0.device) * x0) + (self._1_minus_alpha_bars_sqrt[t].view(-1, 1, 1, 1).to(x0.device) * noise)
         return x_t, noise
 
-    def remove_noise(self, xt: torch.Tensor, t: torch.Tensor, noise: torch.Tensor):
+    # def remove_noise(self, xt: torch.Tensor, t: torch.Tensor, noise: torch.Tensor):
+    #     # DDPM reverse step: x_t -> x_{t-1}
+
+    #     if not torch.is_tensor(t):
+    #         t = torch.tensor([t], device=xt.device, dtype=torch.long)
+    #     elif t.ndim == 0:
+    #         t = t.unsqueeze(0)
+
+    #     alpha_t = self.alphas[t].view(-1, 1, 1, 1).to(xt.device)
+    #     beta_t = self.betas[t].view(-1, 1, 1, 1).to(xt.device)
+    #     alpha_bar_t = self.alpha_bars_cumprod[t].view(-1, 1, 1, 1).to(xt.device)
+
+    #     mean = (
+    #         xt - ((1 - alpha_t) / torch.sqrt(1 - alpha_bar_t)) * noise) / torch.sqrt(alpha_t)
+
+    #     if t[0].item() == 0: return mean
+
+    #     z = torch.randn_like(xt)
+    #     return mean + torch.sqrt(beta_t) * z
+    
+
+    def remove_noise(self, xt, t, noise):
         # DDPM reverse step: x_t -> x_{t-1}
 
         if not torch.is_tensor(t):
@@ -37,14 +58,18 @@ class DDPM:
         elif t.ndim == 0:
             t = t.unsqueeze(0)
 
-        alpha_t = self.alphas[t].view(-1, 1, 1, 1).to(xt.device)
-        beta_t = self.betas[t].view(-1, 1, 1, 1).to(xt.device)
+        alpha_t     = self.alphas[t].view(-1, 1, 1, 1).to(xt.device)
+        beta_t      = self.betas[t].view(-1, 1, 1, 1).to(xt.device)
         alpha_bar_t = self.alpha_bars_cumprod[t].view(-1, 1, 1, 1).to(xt.device)
 
-        mean = (
-            xt - ((1 - alpha_t) / torch.sqrt(1 - alpha_bar_t)) * noise) / torch.sqrt(alpha_t)
+        mean = (xt - ((1 - alpha_t) / torch.sqrt(1 - alpha_bar_t)) * noise) / torch.sqrt(alpha_t)
 
-        if t[0].item() == 0: return mean
+        if t[0].item() == 0:
+            return mean
+
+        # posterior variance
+        alpha_bar_prev = self.alpha_bars_cumprod[t - 1].view(-1, 1, 1, 1).to(xt.device)
+        beta_tilde_t   = (1 - alpha_bar_prev) / (1 - alpha_bar_t) * beta_t
 
         z = torch.randn_like(xt)
-        return mean + torch.sqrt(beta_t) * z
+        return mean + torch.sqrt(beta_tilde_t) * z
