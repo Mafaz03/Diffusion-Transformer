6771249a147dc97895afd068ac89b704c9b74b3c
Mafaz03 <mohdmafaz200303@gmail.com>
2026-06-12 12:20:33 +0530

    not overfitting

--- img_dataloader.py
+++ img_dataloader.py
@@ -39,7 +39,6 @@ class dataset_imgs(Dataset):
         return len(self.all_pths)
 
     def __getitem__(self, index):
-        index = 0
         selected = self.all_pths[index]
 
         img = plt.imread(selected)
