ce4e77bf53ee56599f4bb0d4c67ce32c05283a70
Mafaz03 <mohdmafaz200303@gmail.com>
2026-06-22 01:32:35 +0530

    pretrained vae

--- VAE.py
+++ VAE.py
@@ -119,7 +119,6 @@ class VAE(torch.nn.Module):
         mu, logvar = self.encode(normal_grid = grid)        # each [B, 4, H/8, W/8]
         z = self.reparameterize(mu = mu, logvar = logvar)   # sampling from gausian distribution
         recon_grid = self.decode(compressed_grid = z)
-        return recon_grid, mu, logvar
     
 def vae_loss(recon, x, mu, logvar, kl_weight=1e-4):
     """
